07ab7a84fd30f9548292f2d8cd52283d90b7c969
nsavva38 <nsavva38@gmail.com>
2024-10-22 19:44:16 -0400

    api events on page and user party form functioning

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-// dont forget about STATE Pets R US
-
-
-
-// The base URL is https://fsa-crud-2aa9294fe819.herokuapp.com/api/2409-ftb-et-web-ftb/events
-// The next segment is our cohort name, e.g. /2409-ftb-et-web-ftb
-// The last segment is based on the resource you are trying to access, e.g. /events
-
-//dont forget .json() to make it legible
-
 const state = {
   parties: []
 
 }
-
-
 
 const renderAllParties = async () => {
 
@@ -21,7 +9,6 @@ const renderAllParties = async () => {
 const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2409-ftb-et-web-ftb/events`)
 const events = await response.json();
 
-console.log(events);
 const partyList = document.querySelector(`ol`);
 
 
@@ -29,11 +16,8 @@ const partyEvents = events.data.map((event) => {
   return event
 })
 
-console.log(``);
 
 partyEvents.forEach((party) => {
-  console.log(party.name);
-  console.log(party.date);
 
   dateAndTimeArray = party.date.split(`T`);
   const date = dateAndTimeArray[0];
@@ -61,58 +45,8 @@ renderAllParties();
 
 
 
+// grab the form and post
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// grab the form
-
-
-
-// form stuff where it says to add names and such for a new party is optional
 const form = document.querySelector(`form`);
 
 form.addEventListener(`submit`, (event) => {
@@ -136,13 +70,39 @@ form.addEventListener(`submit`, (event) => {
   inputName.value = ``;
   inputLocation.value = ``;
   inputDescription.value = ``;
-  console.log(state.parties);
-});
+
+
+  const partyEvents = state.parties.map((event) => {
+    return event;
+    
+  })
+
+  partyEvents.forEach((party) => {
+
+  
+    const partyLI = document.createElement(`li`);
+    partyLI.innerHTML =  `
+    <h3>${party.name}</h3>
+      <p>Description: ${party.description}</p>
+      <p>Location: ${party.location}</p>
+    `;
+    
+    const partyList = document.querySelector(`ol`);
+    partyList.append(partyLI);
+    
+  })
+  
+  
+  }
+  
+);
 
 
 
 
-//list of parties from API/events
+
+
+
 
 
 
